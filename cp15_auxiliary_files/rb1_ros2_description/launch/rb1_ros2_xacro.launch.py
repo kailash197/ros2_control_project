e06662dcf56da7d8a6e5ce4ab41e62246e454d59
Kailash Khadka <k.khadka343@gmail.com>
2025-03-03 18:46:33 +0000

    Configure the Differential Drive Controller with ROS2 Control

--- cp15_auxiliary_files/rb1_ros2_description/launch/rb1_ros2_xacro.launch.py
+++ cp15_auxiliary_files/rb1_ros2_description/launch/rb1_ros2_xacro.launch.py
@@ -1,13 +1,13 @@
 import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
 from launch.substitutions import Command, LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from ament_index_python.packages import get_package_prefix
 from launch_ros.descriptions import ParameterValue
-
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler, TimerAction, LogInfo
+from launch.event_handlers import OnProcessExit, OnExecutionComplete, OnProcessStart
 
 def generate_launch_description():
 
@@ -58,9 +58,8 @@ def generate_launch_description():
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
-        namespace=robot_name_1,
-        parameters=[{'frame_prefix': robot_name_1+'/', 'use_sim_time': use_sim_time,
-                     'robot_description': ParameterValue(Command(['xacro ', robot_desc_path, ' robot_name:=', robot_name_1]), value_type=str)}],
+        parameters=[{'use_sim_time': use_sim_time,
+                     'robot_description': ParameterValue(Command(['xacro ', robot_desc_path]), value_type=str)}],
         output="screen"
     )
 
@@ -68,11 +67,54 @@ def generate_launch_description():
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=['-entity', robot_name_1, '-x', '0.0', '-y', '0.0', '-z', '0.0',
-                   '-topic', robot_name_1+'/robot_description']
+                   '-topic', '/robot_description']
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster",
+                   "--controller-manager", "/controller_manager"],
+    )
+
+    diffdrive_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["rb1_base_controller",
+                   "--controller-manager", "/controller_manager"],
     )
 
     return LaunchDescription([
         gazebo,
         rsp_robot1,
-        spawn_robot1,
+        RegisterEventHandler(
+                event_handler=OnProcessStart(
+                    target_action=rsp_robot1,
+                    on_start=[
+                        TimerAction(
+                            period=10.0, # gazebo loading
+                            actions=[spawn_robot1],
+                        )
+                    ]
+                )
+            ),
+        RegisterEventHandler(
+            OnExecutionComplete(
+                target_action=spawn_robot1,
+                on_completion=[
+                    LogInfo(
+                        msg='Spawn finished, waiting 10 seconds to start controllers.'),
+                    TimerAction(
+                        period=10.0,
+                        actions=[joint_state_broadcaster_spawner],
+                    )
+                ]
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[diffdrive_controller_spawner],
+            )
+        ),
     ])
